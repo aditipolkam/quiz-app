cfe495c061be942745e86d96b45d771a08fbb1b7
aditipolkam <aditipolkam@gmail.com>
2023-04-15 15:14:41 +0530

    style: :sparkles: add tailwind and style up mainlayout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div className="px-40 py-8">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between flex-row">
+      <div className="font-bold text-2xl">Quiz App</div>
+      <div>
+        <button className="text-md bg-black text-white px-2 py-1 rounded-md">
+          Create quiz
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
